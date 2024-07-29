65063e2021850fa3292082e811cdfa6383140876
Rhys Miller <rjmills87@aol.com>
2024-07-29 21:11:45 +0100

    Created form element and implemented styling as per design.

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("Hello from script.js");
+
+const ratings = document.querySelectorAll(".rating");
+
+ratings.forEach((rating) => {
+  rating.addEventListener("click", function () {});
+});
