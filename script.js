ce3c39aa5804d02f4a597885c4485d1623256570
Rhys Miller <rjmills87@aol.com>
2024-09-15 21:25:11 +0100

    Added rating submission functionality

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-console.log("Hello from script.js");
-
 const ratingContent = document.getElementById("rating-content");
+const thankyouContent = document.getElementById("thankyou-content");
+const chosenRating = document.getElementById("chosen-rating");
+const submit = document.querySelector(".submit-btn");
+
+// Get the selected value from rating form
+const getRatingValue = () => {
+  const rating = document.getElementsByName("rating");
+  for (i = 0; i < rating.length; i++) {
+    if (rating[i].checked) {
+      chosenRating.innerText = rating[i].value;
+    }
+  }
+};
+
+// Form Submission
+submit.addEventListener("click", function () {
+  getRatingValue();
+  ratingContent.style.display = "none";
+  thankyouContent.style.display = "flex";
+
+  // Prevent page refresh on form submission
+  document.getElementById("ratings").addEventListener("submit", function (e) {
+    e.preventDefault();
+  });
+});
