5adad5c852a38c44a42a11213d34d523470d610d
Rhys Miller <rjmills87@aol.com>
2024-09-16 16:26:05 +0100

    Removed function no longer needed to retrieve form rating value.

--- script.js
+++ script.js
@@ -3,20 +3,10 @@ const thankyouContent = document.getElementById("thankyou-content");
 const chosenRating = document.getElementById("chosen-rating");
 const form = document.getElementById("ratings");
 
-// Get the selected value from rating form
-const getRatingValue = () => {
-  const rating = document.getElementsByName("rating");
-  for (i = 0; i < rating.length; i++) {
-    if (rating[i].checked) {
-      chosenRating.innerText = rating[i].value;
-    }
-  }
-};
-
 // Form Submission
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  getRatingValue();
+  chosenRating.innerText = form.rating.value;
   ratingContent.style.display = "none";
   thankyouContent.style.display = "flex";
 });
